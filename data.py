0ef2332b085363cfd54a710c18096388c466faae
Lakshmi Nair <lakshminair145@gmail.com>
2025-01-31 22:43:46 +0530

    Updated data.py with new code

--- data.py
+++ data.py
@@ -1,4 +1,6 @@
-import pandas as pd  
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 df = pd.read_csv(r'EV_Population.csv')
 print(df)
 df_sorted = df.sort_values(by='Model Year')
@@ -14,5 +16,9 @@ print("vehicles with maximum electric range ")
 print(df_mrg[['Model Year','Make','Electric Range']])
 df_me= df['Make'].value_counts().idxmax()
 print(f"most electric vehicles are made by:{df_me}")
-
+y_c=df_sorted['Model Year'].value_counts().sort_index()
+years = y_c.index
+counts = y_c.values
+plt.plot(years,counts,linestyle='-',color='y')
+plt.show()
 
